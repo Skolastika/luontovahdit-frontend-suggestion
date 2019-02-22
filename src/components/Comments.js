61f569064da82c14cd7cb51787ec755014c7e590
Henna-Riikka Ruonala <hennariikka.ruonala@gmail.com>
2019-02-22 13:22:06 +0200

    Added newest/oldest sorting for comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Segment, Button, Icon, Divider } from 'semantic-ui-react'
+import { Segment, Button, Icon, Divider, Header, Dropdown } from 'semantic-ui-react'
 import { dateFormat } from '../constants'
 import { upVoteComment, downVoteComment } from '../reducers/hotspotReducer'
 import VoteButton from './VoteButton'
@@ -8,11 +8,65 @@ import LoginPromptPopup from './LoginPromptPopup'
 
 class Comments extends React.Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      comments: props.comments
+    }
+  }
+
+  sortComments = (event, data) => {
+    event.preventDefault()
+    const sortedComments = this.state.comments
+    console.log(event.target.value)
+    console.log(event)
+    console.log(data)
+    const sortOrder = data.value === 'vanhin'
+      ? (a, b) => {
+          if (new Date(a.createdAt).getTime() > new Date(b.createdAt).getTime()) {
+            return 1
+          } else {
+            return -1
+          }
+        }
+      : (a, b) => {
+          if (new Date(a.createdAt).getTime() < new Date(b.createdAt).getTime()) {
+            return 1
+          }
+          else {
+            return -1
+          }
+        }
+    sortedComments.sort(sortOrder)
+    this.setState({ comments: sortedComments })
+  }
+
   render() {
     console.log(this.props)
     return (
       <div>
-        { this.props.comments.map(comment =>
+        <div style={{ paddingBottom: '20px' }}>
+        <Header as='h3' floated='left'>Kommentit</Header>
+        <Dropdown basic selection
+          options={[
+            {
+              text: 'Vanhimmat ensin',
+              value: 'vanhin'
+            },
+            {
+              text: 'Uusimmat ensin',
+              value: 'uusin'
+            }
+          ]}
+          defaultValue='vanhin'
+          onChange={ this.sortComments }
+          compact
+          style={{ float: 'right', marginTop: '-5px' }}
+        />
+        </div>
+        <Divider hidden />
+        { this.state.comments.map(comment =>
           <div key={ comment.id }>
             <Segment>
               { new Date(comment.createdAt).toLocaleDateString('fi-FI', dateFormat) } lisännyt { comment.addedBy.displayname }
